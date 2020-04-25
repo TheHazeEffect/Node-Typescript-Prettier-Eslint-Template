ba52485cf93186ba84608a8e7cfd74e1e298fda0
Troy Anderson <TroyAnderson.d@gmail.com>
2020-04-25 00:10:22 -0500

    express server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log('work');
+import express = require('express');
+const port = 3000;
+const app: express.Application = express();
+
+app.get('/', (req, res) => {
+    res.send('hello World');
+});
+
+// eslint-disable-next-line no-console
+app.listen(port, () => console.log(`App Running on Port: ${port}`));
